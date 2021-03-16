f624c4a0251bd2910fa492cb2d8723ab28d7ffda
Sanket Manmode <manmodesanket@gmail.com>
2021-03-16 23:36:26 +0530

    badge added and toggle logic change

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ let toggle = 0;
 const toggleNav = () => {
     if(toggle === 0) {
         const nav = document.querySelector('.component-list');
-        let arr = ['component-list', 'show'];
-        nav.classList.add(...arr);
-        nav.classList.remove('hide');
+        nav.className = 'component-list show';
         toggle = 1;
     }
     else if(toggle === 1) {
         const nav = document.querySelector('.component-list');
-        let arr = ['component-list', 'hide'];
-        nav.classList = ['component-list', 'hide'];
-        nav.classList.remove('show');
-        nav.classList.add(...arr);
+        nav.className = 'component-list hide';
         toggle = 0;
     }
 }
