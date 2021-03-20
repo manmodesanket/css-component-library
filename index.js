014cbf876a9ddcecef1b565f699935990d826eac
Sanket Manmode <manmodesanket@gmail.com>
2021-03-20 15:43:34 +0530

    navbar toggle logic changed

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 
-let toggle = 0;
+
 
 const toggleNav = () => {
-    if(toggle === 0) {
-        const nav = document.querySelector('.component-list');
-        nav.className = 'component-list show';
-        toggle = 1;
-    }
-    else if(toggle === 1) {
-        const nav = document.querySelector('.component-list');
-        nav.className = 'component-list hide';
-        toggle = 0;
-    }
+    const nav = document.querySelector('.component-list');
+    nav.classList.toggle('show');
 }
 
 
